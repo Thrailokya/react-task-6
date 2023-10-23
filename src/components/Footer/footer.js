f3b9a0e921993e8e24be227bf767ffb8f2287736
Thrailokya <thrailokyareddy6655@gmail.com>
2023-10-23 19:00:10 +0530

    first commit

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer =()=>{
+    return(
+        <footer className='footer'>
+            Copyright &#169; 2023 K.L.Thrailokya. All rights reserved.
+        </footer>
+    );
+}
+
+export default Footer
